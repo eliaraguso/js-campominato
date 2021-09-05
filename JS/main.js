ef7761f4efefefc7bc0c4a35632043ec5063e7e0
eliaraguso <elia.raguso@gmail.com>
2021-09-05 14:30:07 +0200

    termine esercizio

--- JS/main.js
+++ JS/main.js
@@ -9,62 +9,95 @@
 // con difficoltà 1 => tra 1 e 80
 // con difficoltà 2 => tra 1 e 50
 
+/**
+ * FUNZIONI
+ */
 
+/**
+ * dato il numero di celle disegna il campo da gioco
+ */
+ function creaCampo(nCelle) {
+	for (var i = 1; i <= nCelle; i++) {
+		// console.log(i);
+		//document.getElementById("campo").innerHTML += "<div class=\"quadrato\">" + i + "</div>";
+		document.getElementById("campo").innerHTML += `<div class="quadrato">${i}</div>`; //template literal
+	}
+}
+/**
+ * dato l'array e l'elemento da cercare, restitusce true se l'elemento è presente altrimenti false
+ */
+function inArray(arr, el) {
+	var count = 0;
 
+	while ( count < arr.length ) {
+		if ( arr[count] == el ) {
+			return true;
+		}
+		
+		count++;
+	}
 
-
-// definisco un'array che conterrà i 16 numeri casuali compresi tra 1  e 100 delle bombe
-
-let numeriBombe = [];
-console.log(numeriBombe);
-
-
-// Genero un array di 16 numeri casuali univoci tra 1 e 100 
-
-while(numeriBombe.length < 16){
-    var r = Math.floor(Math.random() * 100) + 1;
-    if(numeriBombe.indexOf(r) === -1) {
-        numeriBombe.push(r);
-    } 
+	return false;
+}
+/**
+ * dato un range di numeri, restituisce un numero random
+ */
+function getRndInteger(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
+/**
+ * PROGRAMMA PRINCIPALE
+ */
 
-// definisco una variabile per indicare l' id del campo
+// variabili di base
+var numeroCelle = 100; // 100, 80, 50
+var nBombe = 16;
+var possibilita = numeroCelle - nBombe;
 
-let idCampo = "campo";
-
-// Creo una funzione per creare la griglia di gioco
-
-function creaGriglia(cellsNumber, idCampo){
-    for (let i = 1; i <= cellsNumber; i++) {
-    document.getElementById(idCampo).innerHTML += `<div class="quadrato">${i}</div>`;
-    }
-
+// Il computer deve generare 16 numeri casuali tra 1 e 100 (bombe).
+// I numeri non possono essere duplicati.
+var bombe = [];
+// ciclo per 16 volte
+while( bombe.length < nBombe ) {
+	// inserisco il numero nell'array delle bombe
+	var numero = getRndInteger(1, numeroCelle);
+	// se il numero NON è gia presente nell'array delle bombe lo inserisco
+	if ( inArray(bombe, numero) == false ) {
+		bombe.push(numero);
+	}
 }
+console.log(bombe);
 
+// 2 disegno il campo da gioco
+creaCampo(numeroCelle);
+// 3 al click sulle cele del campo
+// In seguito il giocatore clicca sulle celle numerate (non può cliccare più volte sulla stessa cella)
+// La partita termina quando il giocatore clicca su un numero “vietato” o clicca su tutte le celle che non sono delle bombe.
+// Al termine della partita il software deve comunicare il punteggio.
+var numeriValidi = [];
 
-// faccio generare la griglia al mio programma eseguendo la funzione
+document.getElementById("campo").addEventListener("click",
+	function(event) {
+		// event.target l'elemnto del dom su cui ho cliccato
+		// mi salvo il numero della casella
+		var numeroCliccato = parseInt(event.target.innerHTML);
+		// se il numero cliccato è presente nell'array delle bombe, hai perso! il punteggio 
+		// altrimenti se hai già cliccato su questa cella, alert (hai già cliccato su questa cella!)
+		// altrimeni è un numero consentito
+		if ( inArray(bombe, numeroCliccato) == true ) {
+			alert("Hai perso! mi disp!! il tuo punteggio: " + numeriValidi.length);
+			location.reload();
+		} else if ( inArray(numeriValidi, numeroCliccato) == true ) {
+			alert("hai gia cliccato su questo numero! :p");
+		} else {
+			numeriValidi.push(numeroCliccato);
+			event.target.classList.add("cliccato");
 
-creaGriglia(5, "campo");
-
-
-// definisco la funzione che rende rossi i quadrati al click utente
-
-function onClick (event){
-    event.target.classList.add("cliccato");
-    alert("hai cliccato!!");
-}
-
-
-
-// scateno la funzione per applicare l'effetto ON CLICK
-document.getElementById(idCampo).addEventListener("click", onClick);
-
-
-
-
-
-
-
-
-
+			if ( numeriValidi.length == possibilita ) {
+				alert("Bravissimo!! hai finito il gioco!!");
+				location.reload();
+			}
+		}
+	}
+);
